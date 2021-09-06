1411e31ee32e72583d1ae9a1a5342638175b8331
AG <ava.gyx@gmail.com>
2021-09-06 15:47:25 -0600

    updated python script

--- lab1.py
+++ lab1.py
@@ -1,4 +1,13 @@
 import requests
 print(requests.__version__)
 
-requests.get("http://google.com/")
+requests.get("https://www.google.com/")
+
+# display raw source code
+r = requests.get("https://github.com/ava-g/cmput404-lab1/blob/master/lab1.py")
+print(r.text)
+
+# write to file to display retrieved html website
+file = open('out.html','w')
+file.write(r.text)
+file.close()
